53b8cbaef10aa4288fa8c8dcc1f979f487b06d62
Thiago Lehmkuhl Luciano <thiagolehmkuhl42@gmail.com>
2021-05-27 16:13:03 -0300

    Starting ex43: Need this if I mess up the code

--- PTHW/ex42.py
+++ PTHW/ex42.py
@@ -1,6 +1,8 @@
 ## Animal is-a object
 class Animal(object):
-    pass
+    
+    def printing(self):
+        print("This is a animal!")
 
 ## Dog is-a Animal
 class Dog(Animal):
@@ -8,12 +10,20 @@ class Dog(Animal):
     def __init__(self,name):
         ## Dog has-a name
         self.name = name
+    
+    def printing(self):
+        print("This is a Dog")
 
 ## Cat is-a Animal
 class Cat(Animal):
 
     def __init__(self, name):
         ## Cat has-a name
+        self.name = name
+    
+    def printing():
+        print("This is a cat")
+
 
 ## Person is-a object
 class Person(object):
@@ -29,7 +39,7 @@ class Person(object):
 class Employee(Person):
     
     def __init__(self, name, salary):
-        ## ???????????????
+        ## Employee is-a Person that has-a name
         super(Employee, self).__init__(name)
         ## Employee has-a salary
         self.salary = salary
@@ -57,7 +67,7 @@ satan = Cat("Satan")
 mary = Person("Mary")
 
 ## Mary has-a pet "Satan"
-mary.pet = satan
+mary.pet = [satan, rover]
 
 # Frank os a Employee that has-a salary
 frank = Employee("Frank", 120000)
@@ -73,3 +83,7 @@ crouse = Salmon()
 
 ## harry is a Halibut
 harry = Halibut
+
+rover.printing()
+
+mary.pet()
